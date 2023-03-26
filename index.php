af7c61436c3b006529c70610404ca771f04a9147
Sushil Kumar <55022564+skverma618@users.noreply.github.com>
2023-03-26 15:30:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,21 +13,21 @@
 
 	$obj->pagination('students',$join,null,$limit);
 
-	// $obj->insert('students',['student_name'=>'Kumar Sanu','age'=>'22','city'=>'Delhi']);
-	// echo "Insert result is : ";
-	// print_r($obj->getResult());
+	$obj->insert('students',['student_name'=>'Kumar Sanu','age'=>'22','city'=>'Delhi']);
+	echo "Insert result is : ";
+	print_r($obj->getResult());
 
-	// $obj->update('students',['city'=>'Chandigarh'],'city="Goa"');
-	// echo "Update result is : ";
-	// print_r($obj->getResult());
+	$obj->update('students',['city'=>'Chandigarh'],'city="Goa"');
+	echo "Update result is : ";
+	print_r($obj->getResult());
 
-	// $obj->delete('students','age ="20"');
-	// echo "Delete result is : ";
-	// print_r($obj->getResult());
+	$obj->delete('students','age ="20"');
+	echo "Delete result is : ";
+	print_r($obj->getResult());
 
-	// $obj->sql('SELECT * FROM students WHERE age = "20"');
-	// echo "SQL result is : ";
-	// print_r($obj->getResult());
+	$obj->sql('SELECT * FROM students WHERE age = "20"');
+	echo "SQL result is : ";
+	print_r($obj->getResult());
 
 
 
